16b2eea3af000598e14419774c72e1a2b973b7f7
Gani Parrott <ganiparrott@system76-pc.localdomain>
2022-09-06 09:55:25 -0700

    fixed goal generation making small map sizes break

--- model/src/goal_table.rs
+++ model/src/goal_table.rs
@@ -1,5 +1,4 @@
 use std::collections::HashSet;
-use rand::Rng;
 
 use util::vec_ops;
 
@@ -45,12 +44,10 @@ impl GoalTable {
         let mut region: (i32, i32) = (1, 0);
         for _ in 0..count {
             let p_x = (width as i32 / 2) + 
-                  (region.0 * (width as i32 / 4)) +
-                  rand::thread_rng().gen_range((width as i32 / 8)..(width as i32 / 4 - 2)) as i32 * region.0.signum();
+                  (region.0 * (width as i32 / 4));
 
             let p_y = (height as i32 / 2) + 
-                  (region.1 * (height as i32 / 4)) +
-                  rand::thread_rng().gen_range((height as i32 / 8)..(height as i32 / 4 - 2)) as i32 * region.1.signum();
+                  (region.1 * (height as i32 / 4));
 
             self.goals.insert((p_x, p_y));
             region = vec_ops::rotate_left(region);
